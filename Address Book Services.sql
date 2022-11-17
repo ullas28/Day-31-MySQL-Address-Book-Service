522547bbe4618dc701a6d0062f448b6390947e47
unknown <you@example.com>
2022-11-17 19:58:00 +0530

    Added : update contact details using first name

--- Address Book Services.sql
+++ Address Book Services.sql
@@ -14,3 +14,6 @@ create table AddressBook (
     ('Nitish','Priyadarshi','MG Road','Gurgaon','Haryana',345641,18468489745,'np000000@gmail.com'),
     ('Sayyed','Mutallib','Food street','Sagar','Madhya Pradesh',1245678945, 236546,'sm00000000000@gmail.com'),
     ('Madhu','Sudhan S','Malavalli','Mandya','Karnataka',522315,1277777777,'mss0000000000@gmail.com');    
+    select * from AddressBook;
+    update AddressBook set phoneNumber = 4512324565 WHERE firstName = 'Sayyed';
+    update AddressBook set zip = 251235 WHERE firstName = 'Sayyed';

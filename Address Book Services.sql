f917df9059b31f38ec17ae3d343690053f2c8f6a
unknown <you@example.com>
2022-11-17 16:29:35 +0530

    Added : table and columns

--- Address Book Services.sql
+++ Address Book Services.sql
@@ -1,1 +1,11 @@
 create database AddressBookService;
+use AddressBookService;
+create table AddressBook (
+	firstName varchar(20) not null,
+    lastName varchar(20) not null,
+    address varchar(100) not null,
+    city varchar(50) not null,
+    state varchar(50) not null,
+    zip int not null,
+    phoneNumber double not null,
+    email varchar(100) not null);

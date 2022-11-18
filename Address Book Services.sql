c0d142675c95f3221db0c0115937b37592d3e706
unknown <you@example.com>
2022-11-18 20:48:05 +0530

    Added : all retrive queries

--- Address Book Services.sql
+++ Address Book Services.sql
@@ -44,3 +44,15 @@ create table AddressBook (
    insert into addressbookservice.addressbook (firstName, lastName, address, city, state, zip, phoneNumber, email, bookName, contactType)
     values ('Ullas','Kumar k','Hebbal','Bangalore','Karnataka',564131,5454874865,'uk00000000@gmail.com','One','Family'),
     ('Ullas','Kumar k','Hebbal','Bangalore','Karnataka',564131,5454874865,'uk00000000@gmail.com','Two','Friends');
+    select * from AddressBook where city = 'Banaglore' or state = 'Karnataka'; 
+	select * from AddressBook where city = 'Sagar';
+    select * from AddressBook where state = 'Haryana';   
+    select count(firstName) from AddressBook where city = 'Bangalore'; 
+    select count(firstName) from AddressBook where city = 'sagar'; 
+    select count(firstName) from AddressBook where city = 'Gurgaon'; 
+    select count(firstName) from AddressBook where state = 'Karnataka'; 
+    select firstName,city from AddressBook order by city;
+    select firstName,city from AddressBook order by firstName;
+    select count(firstName) from AddressBook where contactType = 'Family'; 
+	select count(firstName) from AddressBook where contactType = 'Friends'; 
+	select count(firstName) from AddressBook where contactType = 'Profession'; 
